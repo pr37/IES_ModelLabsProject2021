85854fd6a954c6a0d459a6483fdc0fcad9d9fa33
Dragana Trivunovic <dragana.trivunovic@vega.local@git config --global user.email dragana.trivunovic@vega.local>
2021-02-13 13:42:11 +0100

    Add project files.

---
+++ NetworkModelService/DataModel/Wires/ACLineSegment.cs
@@ -0,0 +1,89 @@
+﻿using FTN.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FTN.Services.NetworkModelService.DataModel.Wires
+{
+    public class ACLineSegment : Conductor
+    {
+        public ACLineSegment(long globalId) : base(globalId)
+        {
+        }
+
+        public bool Feeder { get; set; }
+
+        public float R { get; set; }
+
+
+
+        public override bool Equals(object obj)
+        {
+            if (base.Equals(obj))
+            {
+                ACLineSegment x = (ACLineSegment)obj;
+                return (x.Feeder == this.Feeder &&
+                        x.R == this.R);
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+
+        public override bool HasProperty(ModelCode t)
+        {
+            switch (t)
+            {
+                case ModelCode.ACLS_FEEDERCABLE:
+                case ModelCode.ACLS_R:
+
+                    return true;
+                default:
+                    return base.HasProperty(t);
+            }
+        }
+
+        public override void GetProperty(Property property)
+        {
+            switch (property.Id)
+            {
+                case ModelCode.ACLS_FEEDERCABLE:
+                    property.SetValue((bool)Feeder);
+                    break;
+
+                case ModelCode.ACLS_R:
+                    property.SetValue(R);
+                    break;
+
+                default:
+                    base.GetProperty(property);
+                    break;
+            }
+        }
+
+        public override void SetProperty(Property property)
+        {
+            switch (property.Id)
+            {
+                case ModelCode.ACLS_FEEDERCABLE:
+                    Feeder = property.AsBool();
+                    break;
+                case ModelCode.ACLS_R:
+                    R = property.AsFloat();
+                    break;
+
+                default:
+                    base.SetProperty(property);
+                    break;
+            }
+        }
+    }
+}
---
+++ NetworkModelService/DataModel/Wires/Conductor.cs
@@ -0,0 +1,90 @@
+﻿using FTN.Common;
+using FTN.Services.NetworkModelService.DataModel.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FTN.Services.NetworkModelService.DataModel.Wires
+{
+    public class Conductor : ConductingEquipment
+    {
+        public Conductor(long globalId) : base(globalId)
+        {
+        }
+        public ConductorMaterialType ConductorMaterial { get; set; }
+
+        public int Length { get; set; }
+
+        
+
+        public override bool Equals(object obj)
+        {
+            if (base.Equals(obj))
+            {
+                Conductor x = (Conductor)obj;
+                return (x.ConductorMaterial == this.ConductorMaterial &&
+                        x.Length == this.Length);
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+
+        public override bool HasProperty(ModelCode t)
+        {
+            switch (t)
+            {
+                case ModelCode.CONDUCTOR_MATERIAL:
+                case ModelCode.CONDUCTOR_LEN:
+
+                    return true;
+                default:
+                    return base.HasProperty(t);
+            }
+        }
+
+        public override void GetProperty(Property property)
+        {
+            switch (property.Id)
+            {
+                case ModelCode.CONDUCTOR_MATERIAL:
+                    property.SetValue((short)ConductorMaterial);
+                    break;
+
+                case ModelCode.CONDUCTOR_LEN:
+                    property.SetValue(Length);
+                    break;
+
+                default:
+                    base.GetProperty(property);
+                    break;
+            }
+        }
+
+        public override void SetProperty(Property property)
+        {
+            switch (property.Id)
+            {
+                case ModelCode.CONDUCTOR_MATERIAL:
+                    ConductorMaterial = (ConductorMaterialType)property.AsEnum();
+                    break;
+                case ModelCode.CONDUCTOR_LEN:
+                    Length = property.AsInt();
+                    break;
+
+                default:
+                    base.SetProperty(property);
+                    break;
+            }
+        }
+
+    }
+}
